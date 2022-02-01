f5a962216d4820db90a8376cc1c5515122eb6a25
jeff1haupt <jhaupt@southbendin.gov>
2022-02-01 18:42:48 -0500

    added seconds counter code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SecondCounterContainer from './second-counter-container';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SecondCounterContainer />
   </React.StrictMode>,
   document.getElementById('root')
 );
